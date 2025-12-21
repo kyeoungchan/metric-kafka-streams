19f8af3edbb0036825889802ccab418c83583b2b
wkc1002 <wkc1002@naver.com>
2025-12-22 07:37:29 +0900

    init

---
+++ src/test/java/com/example/metrickafkastreams/MetricKafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metrickafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricKafkaStreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
